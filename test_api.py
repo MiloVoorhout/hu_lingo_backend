0ac93d9b4cd2d49cd29b0d47d39bc251d11b06d9
milo <milo.voorhout@student.hu.nl>
2020-11-26 16:51:34 +0100

    Moved test swagger file to a better location

--- test_api.py
+++ test_api.py
@@ -2,7 +2,7 @@ import pytest
 import connexion
 
 flask_app = connexion.FlaskApp(__name__)
-flask_app.add_api('lingo/swagger/swagger_test.yaml')
+flask_app.add_api('test/swagger/swagger_test.yaml')
 
 
 @pytest.fixture(scope='module')
