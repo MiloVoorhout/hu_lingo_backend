cd82a08508dff2eeb59ebf61fee8065e4e5f9831
milo <milo.voorhout@student.hu.nl>
2020-12-15 14:34:41 +0100

    Changed app to heroku requirements

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -10,9 +10,9 @@ def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
-                arguments={'title': 'Connexion example'},
+                arguments={'title': 'Connexion api'},
                 pythonic_params=True)
-    app.run(port=8080)
+    app.run()
 
 
 if __name__ == '__main__':
